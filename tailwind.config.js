7cc5bb81e968ac64220a889ad3bccb6256961aaa
Eduardo Guzmán Hermandez <eduardo.guzman@evertecinc.com>
2021-09-16 17:14:13 -0500

    Publishes tailwindcss preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,9 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './docs/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: () => ({
-        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
-      }),
-    },
-  },
+  presets: [
+    require('./src/tailwind.js')
+  ],
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [],
 }
