07aa4bdf59a97e01b0c787f9be2d60b30d23a052
David Valbuena <davidvalbuena99@gmail.com>
2021-09-22 08:00:10 -0500

    breadcrumb

---
+++ docs/views/Breadcrumbs.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="flex flex-col h-full flex-1 justify-center items-center">
+
+      <div class="w-full pb-3">
+        <h2 class="pb-6">Breadcrumb</h2>
+        <table-component :data="dataDescription" />
+      </div>
+      
+      <div class="w-full max-w-xs mx-auto">
+        <div>
+          <h3 class="pb-3">Simple</h3>
+      
+        </div>
+
+      
+
+      </div>
+
+    </div>  
+</template>
+
+<script>
+import { SBreadcrumbs } from "../../src/index.js";
+import TableComponent from "./components/TableComponent.vue";
+
+const dataDescription = [
+  { prop: 'pages', description: 'array of pages', accepted: '-', default: "[]" },
+]
+
+export default {
+  setup() {
+    return {
+      dataDescription,
+    }
+  },
+  components: {
+    TableComponent,
+    SBreadcrumbs,
+  },
+  data() {
+    return {
+      pages: [
+        { name: 'first level', href: '#', current: false },
+        { name: 'second level', href: '#', current: true },
+      ]
+    }
+  },
+}
+</script>
---
+++ src/components/SBreadcrumbs.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="flex max-w-7xl mx-auto sm:px-6 md:px-8 m-2" aria-label="Breadcrumb">
+        <ol role="list" class="flex items-center space-x-4">
+            <li>
+                <div>
+                    <a :href="route('dashboard')" class="text-gray-400 hover:text-gray-500">
+                        <!-- <ShieldCheckIcon class="flex-shrink-0 h-5 w-5" aria-hidden="true" /> -->
+                        <span class="sr-only">Home</span>
+                    </a>
+                </div>
+            </li>
+            <li v-for="page in pages" :key="page.name">
+                <div class="flex items-center">
+                    <!-- <ChevronRightIcon class="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" /> -->
+                    <a :href="page.href" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" :aria-current="page.current ? 'page' : undefined">{{ page.name }}</a>
+                </div>
+            </li>
+        </ol>
+    </nav>
+</template>
+
+<script>
+
+/* import { ChevronRightIcon, ShieldCheckIcon } from '@heroicons/vue/solid' */
+
+const props = defineProps({
+  pages: {
+    type: Array,
+    required: true,
+    default: []
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
