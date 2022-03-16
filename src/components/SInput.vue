047e88ecdce9e0f5662565fcccba90381c1d6def
Eduardo Guzmán H <eduardo.guzman@evertecinc.com>
2022-03-16 08:52:38 -0500

    Updating tailwindcss to v3

--- src/components/SInput.vue
+++ src/components/SInput.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="relative border border-gray-300 rounded-xl px-4 py-2.5 shadow-sm focus-within:ring-1 focus-within:ring-gray-800 focus-within:border-gray-800">
-    <label :for="id" class="absolute -top-2 left-3 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-500">
+  <div
+    class="relative border border-gray-300 rounded-xl px-4 py-2.5 shadow-sm focus-within:ring-1 focus-within:ring-gray-800 focus-within:border-gray-800"
+  >
+    <label
+      :for="id"
+      class="absolute -top-2 left-3 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-500"
+    >
       {{ label }}
     </label>
-    <input 
-      :type="type" 
-      :name="name" 
-      :id="id" 
+    <input
+      :type="type"
+      :name="name"
+      :id="id"
       :placeholder="placeholder"
       :disabled="disabled"
-      :value="modelValue" 
+      :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
-      class="block w-full border-0 p-0 text-gray-900 placeholder-gray-400 focus:ring-0" 
-      autocomplete="off"/>
+      class="block w-full border-0 p-0 text-gray-900 placeholder-gray-400 focus:ring-0"
+      autocomplete="off"
+    />
   </div>
 </template>
 
@@ -21,27 +27,27 @@ const props = defineProps({
   label: {
     type: String,
     required: false,
-    default: undefined
+    default: undefined,
   },
   type: {
     type: String,
     required: false,
-    default: 'text',
+    default: "text",
   },
   name: {
     type: String,
     required: false,
-    default: undefined
+    default: undefined,
   },
   id: {
     type: String,
     required: false,
-    default: undefined
+    default: undefined,
   },
   placeholder: {
     type: String,
     required: false,
-    default: undefined
+    default: undefined,
   },
   disabled: {
     type: Boolean,
@@ -49,6 +55,5 @@ const props = defineProps({
     default: false,
   },
   modelValue: String,
-})
-
+});
 </script>
