bf94fa9b0ba2049dd9a8fbe58fb6f6b201654697
Susana <susana.santamaria@evertecinc.com>
2022-04-13 11:35:57 -0500

    Fix padding and gap for header

--- src/components/SDescriptionList.vue
+++ src/components/SDescriptionList.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="flex flex-col bg-white rounded-2xl shadow overflow-hidden">
     <div class="flex flex-row items-center justify-between p-5 gap-2.5">
-      <div class="flex flex-col flex-1">
-        <h3 class="text-lg leading-6 font-medium text-gray-900">{{ title }}</h3>
-        <p class="mt-1 max-w-2xl text-sm text-gray-500">{{ description }}</p>
+      <div class="flex flex-col flex-1 gap-1">
+        <h3 class="text-lg font-semibold text-gray-900">{{ title }}</h3>
+        <p class="text-base font-medium text-gray-500">{{ description }}</p>
       </div>
-      <div class="flex flex-col flex-shrink-0">
+      <div class="flex flex-col">
         <div class="flex flex-row gap-3 items-center">
           <slot name="actions"/>
         </div>
       </div>
     </div>
 
-    <div class="flex flex-col gap-8 p-5 border-t border-gray-200 px-4 py-5 sm:px-6">
+    <div class="flex flex-col gap-8 p-5">
       <dl class="grid grid-cols-2">
         <div class="flex flex-col gap-8">
           <slot name="col-1"/>
