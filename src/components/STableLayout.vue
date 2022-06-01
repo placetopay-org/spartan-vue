4d4f839c605e26102a4e39c1b58dc9df7243f088
Susana <susana.santamaria@evertecinc.com>
2022-06-01 20:57:13 +0000

    Remove margin to from STable

--- src/components/STableLayout.vue
+++ src/components/STableLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="-mx-4 mt-8 overflow-hidden rounded-2xl shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0">
+  <div class="-mx-4 overflow-hidden rounded-2xl shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0">
     <slot />
   </div>
 </template>
