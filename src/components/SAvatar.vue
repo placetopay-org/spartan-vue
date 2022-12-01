98291c23f400b2a40e8b109cfea9597e1184af80
Meiyer Jaimes <meiyerdev@DESKTOP-2IOOLDE.localdomain>
2022-12-01 15:11:36 -0400

    feat: add multi sizes to SAvatar component

--- src/components/SAvatar.vue
+++ src/components/SAvatar.vue
@@ -1,5 +1,5 @@
 <template>
-  <img class="h-8 w-8 rounded-full" :src="src" />
+  <img class="rounded-full" :src="src" :class="sizeClass" />
 </template>
 
 <script>
@@ -9,11 +9,27 @@ export default {
       type: String,
       required: true,
     },
+    size: {
+      type: String,
+      default: 'sm',
+      validator: (value) => ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl'].includes(value),
+    },
   },
   computed: {
     src() {
       return `https://ui-avatars.com/api/?name=${this.name}`;
     },
+    sizeClass() {
+      return {
+        xs: 'h-4 w-4',
+        sm: 'h-8 w-8',
+        md: 'h-12 w-12',
+        lg: 'h-16 w-16',
+        xl: 'h-20 w-20',
+        '2xl': 'h-24 w-24',
+        '3xl': 'h-32 w-32',
+      }[this.size];
+    }
   },
 };
 </script>
