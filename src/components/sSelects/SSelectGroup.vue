fc7c8706a0e834930b5f1c70e0a16d4454c40b1b
Meiyer jaimes <meiyerjaimes@outlook.com>
2023-01-25 19:01:52 -0500

    wip

--- src/components/sSelects/SSelectGroup.vue
+++ src/components/sSelects/SSelectGroup.vue
@@ -9,7 +9,6 @@ const emits = defineEmits<{
 }>();
 const props = defineProps<{
   label?: string;
-  name?: string;
   id?: string;
   placeholder?: string,
   disabled?: boolean,
@@ -33,65 +32,64 @@ const model = computed({
 </script>
 
 <template>
-    <Listbox as="div" v-slot="{ open }" v-model="model" :disabled="disabled">
+    <Listbox as="div" v-slot="{ open }" v-model="model" :disabled="disabled" class="relative">
         <ListboxLabel v-if="label && id" :for="id" class="block text-sm font-medium text-gray-700">
             {{ label }}
         </ListboxLabel>
 
-        <div class="relative mt-1">
-            <ListboxButton
-                class="flex items-center justify-between w-full px-3 pt-2 pb-2 text-base text-left text-gray-900 placeholder-gray-500 border border-gray-300 rounded-lg shadow-sm cursor-auto focus:z-10 focus:border-primary-300 focus:ring focus:ring-primary-100 disabled:opacity-70"
+        <ListboxButton
+            class="flex items-center justify-between w-full px-3 pt-2 pb-2 text-base text-left text-gray-900 placeholder-gray-500 border border-gray-300 rounded-lg shadow-sm cursor-auto focus:z-10 focus:border-primary-300 focus:ring focus:ring-primary-100 disabled:opacity-70"
+            :class="[label && id ? 'mt-1' : '']"
+        >
+            <span :class="['flex items-center truncate', model ? 'uppercase' : '']">{{ model ? model.value : placeholder }}</span>
+
+            <span class="flex items-center pointer-events-none">
+                <ChevronDownIcon
+                    class="w-4 h-4 text-gray-500"
+                    :aria-hidden="open ? 'true' : 'false'"
+                />
+            </span>
+        </ListboxButton>
+        
+        <transition
+            enter-active-class="transition duration-100 ease-out"
+            enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100"
+            leave-active-class="transition duration-75 ease-in"
+            leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-95 opacity-0"
+        >
+            <ListboxOptions
+                class="absolute w-full mt-1 overflow-auto text-base bg-white rounded-lg shadow-lg min-w-56 min-h-56 max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
             >
-                <span :class="['flex items-center truncate', model ? 'uppercase' : '']">{{ model ? model.value : placeholder }}</span>
+                <li v-for="group in options" :key="group.label">
+                    <div v-if="group.label" class="px-4 py-1 bg-gray-100">
+                        <span class="font-semibold text-gray-700">{{ group.label }}</span>
+                    </div>
 
-                <span class="flex items-center pointer-events-none">
-                    <ChevronDownIcon
-                        class="w-4 h-4 text-gray-500"
-                        :aria-hidden="open ? 'true' : 'false'"
-                    />
-                </span>
-            </ListboxButton>
-            
-            <transition
-                enter-active-class="transition duration-100 ease-out"
-                enter-from-class="transform scale-95 opacity-0"
-                enter-to-class="transform scale-100 opacity-100"
-                leave-active-class="transition duration-75 ease-in"
-                leave-from-class="transform scale-100 opacity-100"
-                leave-to-class="transform scale-95 opacity-0"
-            >
-                <ListboxOptions
-                    class="absolute w-full mt-1 overflow-auto text-base bg-white rounded-lg shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-                >
-                    <li v-for="group in options" :key="group.label">
-                        <div v-if="group.label" class="px-4 py-1 bg-gray-100">
-                            <span class="font-semibold text-gray-700">{{ group.label }}</span>
-                        </div>
+                    <ul>
+                        <ListboxOption
+                            as="template"
+                            v-for="option in group.options"
+                            :key="option.value"
+                            :value="option"
+                            v-slot="{ active, selected }"
+                        >
+                            <li :class="[active ? 'bg-gray-200 bg-opacity-20' : 'text-gray-900', 'relative cursor-default select-none py-2 px-4']">
+                                <div class="flex items-center gap-1">
+                                    <span class="font-semibold uppercase">{{ option.value }}:</span>
+                                    <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">{{ option.label }}</span>
+                                </div>
 
-                        <ul>
-                            <ListboxOption
-                                as="template"
-                                v-for="option in group.options"
-                                :key="option.value"
-                                :value="option"
-                                v-slot="{ active, selected }"
-                            >
-                                <li :class="[active ? 'bg-gray-200 bg-opacity-20' : 'text-gray-900', 'relative cursor-default select-none py-2 px-4']">
-                                    <div class="flex items-center gap-1">
-                                        <span class="font-semibold uppercase">{{ option.value }}:</span>
-                                        <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">{{ option.label }}</span>
-                                    </div>
-
-                                    
-                                    <span v-if="selected" class="absolute inset-y-0 right-0 flex items-center pr-4 text-gray-600">
-                                        <CheckIcon class="w-5 h-5" aria-hidden="true" />
-                                    </span>
-                                </li>
-                            </ListboxOption>
-                        </ul>
-                    </li>
-                </ListboxOptions>
-            </transition>
-        </div>
+                                
+                                <span v-if="selected" class="absolute inset-y-0 right-0 flex items-center pr-4 text-gray-600">
+                                    <CheckIcon class="w-5 h-5" aria-hidden="true" />
+                                </span>
+                            </li>
+                        </ListboxOption>
+                    </ul>
+                </li>
+            </ListboxOptions>
+        </transition>
     </Listbox>
 </template>
