c8d0a76b69489e6a7e7229e37a4645682cd7d6ac
Brayhan Garcia <58666056+Brayha@users.noreply.github.com>
2023-05-17 14:40:10 -0500

    Se agrego introduccion del sistema de diseño

--- src/stories/Page.vue
+++ src/stories/Page.vue
@@ -1,69 +1,97 @@
 <template>
   <article>
-    <my-header
+    <!--  <my-header
       :user="user"
       @login="$emit('login')"
       @logout="$emit('logout')"
       @createAccount="$emit('createAccount')"
-    />
+    /> -->
 
     <section>
-      <h2>Pages in Storybook</h2>
+      <img src="./assets/cover.jpg" />
+      <hr />
+      <h2>Desing System</h2>
       <p>
-        We recommend building UIs with a
-        <a href="https://componentdriven.org" target="_blank" rel="noopener noreferrer">
-          <strong>component-driven</strong>
+        Hemos desarrollado un innovador sistema de diseño llamado
+        <strong>"Spartan"</strong> que se utiliza tanto en proyectos actuales
+        como en futuros.
+      </p>
+
+      <p>
+        Nuestro objetivo al crear este sistema fue unificar la línea gráfica en
+        todos los proyectos. En nuestro proceso de diseño y desarrollo, nos
+        basamos en el framework llamado
+        <a
+          href="https://tailwindui.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <strong>Tailwind,</strong>
         </a>
-        process starting with atomic components and ending with pages.
+        que cuenta con sus propios componentes.
       </p>
+
       <p>
-        Render pages with mock data. This makes it easy to build and review page states without
-        needing to navigate to them in your app. Here are some handy patterns for managing page data
-        in Storybook:
+        Esto nos permitió ahorrar mucho tiempo en el desarrollo de nuestro
+        propio sistema de diseño, ya que utilizamos este framework como base
+        para crear uno más personalizado.
       </p>
+
+      <h3>Diseño</h3>
+      <p>
+        Nuestro sistema de diseño viene ya diseñado desde la plataforma
+        <a
+          href="https://www.figma.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <strong>figma</strong>
+        </a>
+        el cual pueden visualizarlo en un navegador o
+        <a
+          href="https://www.figma.com/downloads/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <strong>instalar el software de manera gratuita</strong>
+        </a>
+      </p>
+
+      <h4>Archivos:</h4>
       <ul>
         <li>
-          Use a higher-level connected component. Storybook helps you compose such data from the
-          "args" of child component stories
+          <strong>1. Spartan:</strong> Sistema de diseño para todos los
+          proyectos, esta en constante actualización
+          <a
+            href="https://www.figma.com/file/hRypwsAfjK2e0g9DOKLROV/Spartan-V2?type=design&node-id=0%3A1&t=V9epUvhbpmJR5icZ-1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <strong>Ver archivo</strong>
+          </a>
         </li>
         <li>
-          Assemble data in the page component from your services. You can mock these services out
-          using Storybook.
+          <strong>2. Panel v2:</strong> Es el nuevo proyecto del nuevo panel
+          para la administración de comercios
+          <a
+            href="https://www.figma.com/file/UIzkf2Ot20xpg7kJmPx65L/Panel-v2?type=design&node-id=0%3A1&t=QGGIyonzBEM4ICDz-1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <strong>Ver archivo</strong>
+          </a>
         </li>
       </ul>
-      <p>
-        Get a guided tutorial on component-driven development at
-        <a href="https://storybook.js.org/tutorials/" target="_blank" rel="noopener noreferrer"
-          >Storybook tutorials</a
-        >
-        . Read more in the
-        <a href="https://storybook.js.org/docs" target="_blank" rel="noopener noreferrer">docs</a>
-        .
-      </p>
-      <div class="tip-wrapper">
-        <span class="tip">Tip</span>
-        Adjust the width of the canvas with the
-        <svg width="10" height="10" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-          <g fill="none" fill-rule="evenodd">
-            <path
-              d="M1.5 5.2h4.8c.3 0 .5.2.5.4v5.1c-.1.2-.3.3-.4.3H1.4a.5.5 0 01-.5-.4V5.7c0-.3.2-.5.5-.5zm0-2.1h6.9c.3 0 .5.2.5.4v7a.5.5 0 01-1 0V4H1.5a.5.5 0 010-1zm0-2.1h9c.3 0 .5.2.5.4v9.1a.5.5 0 01-1 0V2H1.5a.5.5 0 010-1zm4.3 5.2H2V10h3.8V6.2z"
-              id="a"
-              fill="#999"
-            />
-          </g>
-        </svg>
-        Viewports addon in the toolbar
-      </div>
     </section>
   </article>
 </template>
 
 <script>
-import './page.css';
-import MyHeader from './Header.vue';
+import "./page.css";
+import MyHeader from "./Header.vue";
 
 export default {
-  name: 'my-page',
+  name: "my-page",
 
   components: { MyHeader },
 
@@ -73,6 +101,6 @@ export default {
     },
   },
 
-  emits: ['login', 'logout', 'createAccount'],
+  emits: ["login", "logout", "createAccount"],
 };
 </script>
