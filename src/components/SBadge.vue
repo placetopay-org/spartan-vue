0627882bd4afe1efa096c578dbd4bc437c4ba40f
Brayhan Garcia <58666056+Brayha@users.noreply.github.com>
2023-05-23 15:06:26 -0500

    Se creo tabs line

--- src/components/SBadge.vue
+++ src/components/SBadge.vue
@@ -7,6 +7,7 @@
 <script>
 const colorOptions = {
   gray: "bg-gray-100 text-gray-800",
+  orange: "bg-primary-100 text-primary-800",
   green: "bg-green-100 text-green-800",
   red: "bg-red-100 text-red-800",
   yellow: "bg-yellow-100 text-yellow-800",
