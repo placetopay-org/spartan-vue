f268d1d76a670d0f55528e0e2f1076d620faa3c0
Brayhan Garcia <58666056+Brayha@users.noreply.github.com>
2023-05-29 15:48:06 -0500

    Se ajusto estilos y se agrego titulo y descripcion de los toggles

--- src/stories/SToggle.stories.js
+++ src/stories/SToggle.stories.js
@@ -11,7 +11,15 @@ const Template = (args) => ({
     return { args };
   },
   template: `
-    <SToggle v-model=""></SToggle>
+    <SToggle v-bind="args">
+     <template v-if="!!args.title" v-slot:title>
+      {{ args.title }}
+     </template>
+
+     <tem plate v-if="!!args.description" v-slot:description>
+      {{ args.description }}
+     </template>
+    </SToggle>
     `,
 });
 
@@ -19,4 +27,19 @@ export const Simple = Template.bind({});
 
 Simple.args = {
   modelValue: true,
+};
+
+export const WithTitle = Template.bind({});
+
+WithTitle.args = {
+  modelValue: true,
+  title: "Create Sites",
+};
+
+export const WithDescription = Template.bind({});
+
+WithDescription.args = {
+  modelValue: true,
+  title: "Create Sites",
+  description: "User will be able to create new sites.",
 };
