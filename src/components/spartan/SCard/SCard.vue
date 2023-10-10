40eaf568506a12bbd4eba185e5951b994d1cc667
JuanZea <vikzea@gmail.com>
2023-10-10 14:51:06 -0500

    fix: dispatch onClick action on SCard

--- src/components/spartan/SCard/SCard.vue
+++ src/components/spartan/SCard/SCard.vue
@@ -63,6 +63,7 @@ const accentStyle = computed(() => {
                     v-for="action in actions"
                     :key="action.text"
                     class="flex w-full items-center justify-center gap-2 px-8 py-4 font-medium text-gray-400 hover:bg-gray-50 focus:outline-none"
+                    @click="action.onClick"
                 >
                     <component :is="action.icon" class="h-5 w-5" />
                     <span>{{ action.text }}</span>
