b1b693e9637b6c78bb16cfde5edc48bf7a5747df
JuanZea <vikzea@gmail.com>
2024-01-30 16:23:05 -0500

    feat: SAvatar types

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 ### Added
+- `SAvatar` now exports his types.
 - `SAccordion` now exports his types.
 
 ## 2.0.0-beta.57 - 2024-01-30
---
+++ src/components/spartan/SAvatar/types.ts
@@ -0,0 +1,9 @@
+export type TAvatarProps = {
+    class: string;
+    borderless: boolean;
+    indicator: boolean;
+    indicatorPosition: 'left-top' | 'left-bottom' | 'right-top' | 'right-bottom';
+    name: string;
+    size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+    src: string;
+};
