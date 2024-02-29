c95553f2fa2c839aad9a52ba726bdc20655c39d2
JuanZea <vikzea@gmail.com>
2024-02-29 13:15:30 -0500

    wip

--- src/stories/examples/AccountsDemo/AccountsDemo.vue
+++ src/stories/examples/AccountsDemo/AccountsDemo.vue
@@ -4,12 +4,19 @@ import VFooter from './VFooter.vue';
 </script>
 
 <template>
-    <div class="flex h-full w-full flex-1">
-        <div class="relative hidden w-0 flex-1 lg:block">
-            <img class="absolute inset-0 h-full w-full object-cover" src="./bento-grid.png" alt="bento grid">
+    <div class="flex h-full w-full flex-1 bg-white">
+        <div class="relative hidden flex-1 lg:grid bg-black rounded-3xl m-6 mr-0 grid-cols-3 gap-1.5 text-white">
+            <div class="flex flex-col gap-1.5 relative overflow-auto">
+                <div class="bg-red-500 rounded-2xl h-[calc(100% + )]">1</div>
+                <div class="bg-red-500 rounded-2xl h-[calc(100% + )]">2</div>
+                <div class="bg-red-500 rounded-2xl h-[calc(100% + )]">3</div>
+                <div class="bg-red-500 rounded-2xl h-[calc(100% + )]">4</div>
+            </div>
+            <div class="bg-red-500">2</div>
+            <div class="bg-red-500">3</div>
         </div>
 
-        <section class="px-4 sm:px-16 flex flex-col justify-center items-center bg-white flex-1 relative">
+        <section class="px-4 sm:px-16 flex flex-col justify-center items-center bg-white flex-1 lg:flex-none relative">
             <div class="mx-auto w-full max-w-sm lg:w-96">
                 <VForm />
             </div>
