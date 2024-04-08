c72a5fea77d178ebceb5ebe735a6207f41e37608
JuanZea <vikzea@gmail.com>
2024-04-08 16:31:49 -0500

    feat: SBreadcrumbs tests

--- vite.config.js
+++ vite.config.js
@@ -60,7 +60,9 @@ export default defineConfig({
         },
     },
     test: {
+        globals: true,
         include: ['./src/components/spartan/**/*.test.ts'],
+        setupFiles: './vitest-setup.ts',
         environment: 'jsdom',
         coverage: {
             reporter: ['lcov', 'html'],
