16be8514fa791922459c75fbaa6c14f35fa2d7cc
JuanZea <vikzea@gmail.com>
2024-07-16 10:14:46 -0500

    Create SFilter.test.ts

---
+++ src/components/spartan/SFilter/SFilter.test.ts
@@ -0,0 +1,91 @@
+import { test, describe } from 'vitest';
+import { render } from '@testing-library/vue';
+import { screen } from '@testing-library/dom';
+import SFilter from './SFilter.vue';
+import userEvent from '@testing-library/user-event';
+import { h } from 'vue';
+
+describe('SFilter', () => {
+    test('Can be rendered', async () => {
+        // Act
+        render(SFilter);
+
+        // Assert
+        screen.getByRole('button', { name: '$spartan.filter.applyBtn' });
+        screen.getByRole('button', { name: '$spartan.filter.clearBtn' });
+    });
+
+    test('Can be rendered a field', async () => {
+        // Act
+        render(SFilter, {
+            props: {
+                immediateApply: true,
+                fields: [{
+                    id: 'brand',
+                    name: 'Brand',
+                    interfaces: {
+                        options: {
+                            multiple: true,
+                            operators: ['equal', 'notEqual', 'contains'],
+                            options: [{id: '1', label: 'Nike'}, 'Adidas', 'Puma', 'Reebok', 'Under Armour'],
+                            customOperators: [{ id: 'custom', label: 'customOper' }],
+                        },
+                    },
+                    state: {
+                        operator: 'equal',
+                        value: ['Adidas'],
+                    },
+                }]
+            }
+        });
+
+        // Assert
+        screen.getByText('Brand |');
+        screen.getByText('Adidas');
+        screen.getByRole('button', { name: 'Remove' });
+    });
+
+    test('Can be update a field', async () => {
+        // Arrange
+        const fields = [];
+        const user = userEvent.setup();
+
+        // Act
+        render(SFilter, {
+            props: {
+                onApply: (fields: any) => console.log(fields),
+                fields: [{
+                    id: 'brand',
+                    name: 'Brand',
+                    interfaces: {
+                        options: {
+                            multiple: true,
+                            operators: ['equal', 'notEqual', 'contains'],
+                            options: [{id: '1', label: 'Nike'}, 'Adidas', 'Puma', 'Reebok', 'Under Armour'],
+                            customOperators: [{ id: 'custom', label: 'customOper' }],
+                        },
+                    },
+                    state: {
+                        operator: 'equal',
+                        value: ['Adidas'],
+                    },
+                }]
+            }
+        });
+
+        const filterBadge = screen.getByRole('button', { name: 'Brand | Adidas Remove' });
+
+        await user.click(filterBadge);
+
+        await user.click(screen.getByLabelText('Puma'));
+
+        await user.click(screen.getByRole('button', { name: '$spartan.filter.addBtn' }));
+
+        await user.click(screen.getByRole('button', { name: '$spartan.filter.applyBtn' }));
+        
+
+        // Assert
+        screen.debug();
+        screen.getByText('Adidas,Puma');
+    });
+});
