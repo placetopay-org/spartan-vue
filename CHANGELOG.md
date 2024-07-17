265bd2ae84b8ee17e5cc458de6dd11e13f244927
JuanZea <vikzea@gmail.com>
2024-07-17 15:10:00 -0500

    feat: SDropdown tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 ### Added
+- `SDataTable` tests.
+- `SFilter` tests.
+- `SDropdown` tests.
 - `SSteps` and `SStepsItem` tests.
 
 ## 2.0.0-beta.87 - 2024-07-15
---
+++ src/components/spartan/SDropdown/SDropdown.test.ts
@@ -0,0 +1,52 @@
+import { test, describe } from 'vitest';
+import { render } from '@testing-library/vue';
+import { screen } from '@testing-library/dom';
+import SDropdown from './SDropdown.vue';
+import SDropdownItem from './SDropdownItem.vue';
+import userEvent from '@testing-library/user-event';
+import { table } from '@/data';
+import { h } from 'vue';
+
+describe('SDropdown', () => {
+    test('Can be rendered', async () => {
+        // Arrange
+        const user = userEvent.setup();
+
+        const Item1 = h(
+            SDropdownItem,
+            null,
+            { default: () => 'Title', description: () => 'Description' },
+        );
+        const Item2 = h(
+            SDropdownItem,
+            null,
+            { default: () => 'Option', description: () => 'Device' },
+        );
+        const Item3 = h(
+            SDropdownItem,
+            null,
+            { default: () => 'Profile', description: () => 'Edit' },
+        );
+
+        // Act
+        render(SDropdown, {
+            slots: {
+                default: [Item1, Item2, Item3],
+                reference: () => 'Dropdown Button',
+            },
+        });
+
+        await user.click(screen.getByRole('button', { name: 'Dropdown Button' }));
+
+        // Assert
+        screen.getByText('Title');
+        screen.getByText('Description');
+
+        screen.getByText('Option');
+        screen.getByText('Device');
+        
+        screen.getByText('Option');
+        screen.getByText('Edit');
+        screen.debug();
+    });
+});
