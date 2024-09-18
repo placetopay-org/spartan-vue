98d112ed359e57f5ba676846ffbdefb8cfe7917b
Juan Zea <juan.zea@evertecinc.com>
2024-09-18 16:54:57 -0500

    fix: export SDColumn

--- src/components/spartan/SDTable/index.ts
+++ src/components/spartan/SDTable/index.ts
@@ -1,3 +1,4 @@
 export { default as SDTable } from './SDTable.vue';
+export { default as SDColumn } from './SDColumn.vue';
 
 export type { TDTableProps, TDColumnProps, TDTableEmits } from './types';
