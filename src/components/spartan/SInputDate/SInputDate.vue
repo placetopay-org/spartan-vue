24eead7a1affea1e5aa4ad720557156ba27ea930
Juan Zea <juan.zea@evertecinc.com>
2024-10-15 15:17:19 -0500

    fix: input date styles

--- src/components/spartan/SInputDate/SInputDate.vue
+++ src/components/spartan/SInputDate/SInputDate.vue
@@ -42,9 +42,10 @@ input[aria-label='Datepicker input'] {
 }
 
 .dp__theme_light {
-    --dp-spartan-primary-color: rgb(var(--color-spartan-primary-500));
+    --dp-primary-color: rgb(var(--color-spartan-primary-500));
     --dp-border-color: #d1d5db;
     --dp-border-color-hover: #d1d5db;
     --dp-icon-color: #9ca3af;
+    --dp-danger-color: transparent;
 }
 </style>
