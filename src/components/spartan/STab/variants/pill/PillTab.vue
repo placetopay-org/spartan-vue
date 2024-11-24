961ddf79119f8bddde30ac9a79befe1a3dc7a5f9
Juan Zea <juan.zea@evertecinc.com>
2024-11-24 14:25:00 -0500

    fix: class props on tabs

--- src/components/spartan/STab/variants/pill/PillTab.vue
+++ src/components/spartan/STab/variants/pill/PillTab.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { usePassthrough } from '@/helpers';
 import { twMerge } from 'tailwind-merge';
+import type { TTabProps } from '../../types';
+
+defineProps<TTabProps>();
 
 const { pt, extractor } = usePassthrough();
 
@@ -9,7 +12,7 @@ const [tabClass, tabProps] = extractor(pt.value.tab);
 </script>
 
 <template>
-    <nav data-s-tabContainer v-bind="tabContainerProps" :class="twMerge(tabContainerClass, String($props.class))" aria-label="Tabs">
+    <nav data-s-tabContainer v-bind="tabContainerProps" :class="twMerge(tabContainerClass, $props.class)" aria-label="Tabs">
         <ul data-s-tab v-bind="tabProps" :class="twMerge('flex space-x-4', tabClass)">
             <slot />
         </ul>
