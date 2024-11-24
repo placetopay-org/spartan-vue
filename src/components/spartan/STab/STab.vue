2bab6af699b6858761c0cdaee24efe821e0b502d
Juan Zea <juan.zea@evertecinc.com>
2024-11-24 15:10:29 -0500

    Update STab.vue

--- src/components/spartan/STab/STab.vue
+++ src/components/spartan/STab/STab.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { TTabProps, TTabEmits } from './types';
 import { createContext } from './api';
+import { twMerge } from 'tailwind-merge';
 
 const emit = defineEmits<TTabEmits>();
 
@@ -10,7 +11,7 @@ const context = createContext(props, emit);
 </script>
 
 <template>
-    <component :is="context.variant.tab">
+    <component :is="context.variant.tab" :class="twMerge($props.class)">
         <slot />
     </component>
 </template>
