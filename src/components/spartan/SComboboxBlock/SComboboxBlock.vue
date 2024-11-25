9bf9cb70253ea947c5dbf926ff3d0c00708b6a66
Juan Zea <juan.zea@evertecinc.com>
2024-11-25 15:01:26 -0500

    Update SComboboxBlock.vue

--- src/components/spartan/SComboboxBlock/SComboboxBlock.vue
+++ src/components/spartan/SComboboxBlock/SComboboxBlock.vue
@@ -2,7 +2,6 @@
 import { SCombobox, type TComboboxProps, type TComboboxEmits } from '../SCombobox';
 import { BlockWrapper, type TBlockWrapperProps } from '@internal';
 import { extractWrapperProps } from '@/helpers';
-import { twMerge } from 'tailwind-merge';
 
 defineEmits<TComboboxEmits>();
 const props = defineProps<Partial<TBlockWrapperProps> & TComboboxProps>();
@@ -13,7 +12,7 @@ const [blockWrapperProps, comboboxProps] = extractWrapperProps<TComboboxProps>(p
     <BlockWrapper wrapper="SComboboxBlock" v-bind="blockWrapperProps" v-slot="{ id }">
         <SCombobox
             :id="id"
-            :class="twMerge('w-full', comboboxProps.class)"
+            class="w-full"
             v-bind="comboboxProps"
             @update:model-value="(newValue) => $emit('update:modelValue', newValue)"
             @query="$emit('query', $event)"
