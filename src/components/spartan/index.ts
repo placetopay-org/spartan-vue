80a3d538c1411c4644d3d5362624d14c93ec4761
Juan Zea <juan.zea@evertecinc.com>
2024-11-26 11:37:21 -0500

    remove: data table component

--- src/components/spartan/index.ts
+++ src/components/spartan/index.ts
@@ -35,7 +35,6 @@ export * from './SInputAmountBlock';
 export * from './SSidebar';
 export * from './STab';
 export * from './STable';
-export * from './SDataTable';
 export * from './SInputIncrement';
 export * from './SInputIncrementBlock';
 export * from './SSkeleton';
