8e59187a224d0e250bdb9c373e45a6a12eab3ff8
Juan Zea <juan.zea@evertecinc.com>
2024-12-09 09:41:42 -0500

    fix: relative td

--- src/components/spartan/SDTable/styles.ts
+++ src/components/spartan/SDTable/styles.ts
@@ -8,7 +8,7 @@ export const tableStyles = cva('w-full overflow-hidden', {
 });
 export type TTableStyles = VariantProps<typeof tableStyles>;
 
-export const cellStyles = cva('', {
+export const cellStyles = cva('relative', {
     variants: {
         unstyled: cbv('p-0', 'first:pl-5 last:pr-5 p-3.5 text-sm text-left'),
         head: cbv('text-gray-900 font-semibold', 'text-gray-500'),
