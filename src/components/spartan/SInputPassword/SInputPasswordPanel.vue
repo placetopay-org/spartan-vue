79483c66a14a30fe1aceaa64fda978dca3479554
Juan Zea <juan.zea@evertecinc.com>
2025-02-05 17:02:50 -0500

    Update SInputPasswordPanel.vue

--- src/components/spartan/SInputPassword/SInputPasswordPanel.vue
+++ src/components/spartan/SInputPassword/SInputPasswordPanel.vue
@@ -2,16 +2,17 @@
 import type { TState } from './types';
 import { CheckCircleIcon } from '@heroicons/vue/24/solid';
 import { translator } from '@/helpers';
+import { twMerge } from 'tailwind-merge';
 
-defineProps<{ state: TState }>();
+defineProps<{ class?: string; state: TState }>();
 
 const { t } = translator('inputPasswordPanel');
 </script>
 
 <template>
-    <div>
+    <div :class="twMerge('space-y-2', $props.class)">
         <div v-for="(value, key) of state" class="flex items-center gap-2">
-            <CheckCircleIcon :class="value?.isValid ? 'text-green-400' : 'text-gray-300'" class="h-6 w-6" />
+            <CheckCircleIcon :class="value?.isValid ? 'text-emerald-400' : 'text-gray-300'" class="h-6 w-6" />
             <span class="text-sm text-gray-600">{{ t(key, {value: value?.value}) }}</span>
         </div>
     </div>
