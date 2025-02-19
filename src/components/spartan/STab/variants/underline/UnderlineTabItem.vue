ecae55f5c5cf17e45bd0b4f0bc4e142db668ec61
Juan Zea <juan.zea@evertecinc.com>
2025-02-19 15:50:00 -0500

    fix: update tabs styles

--- src/components/spartan/STab/variants/underline/UnderlineTabItem.vue
+++ src/components/spartan/STab/variants/underline/UnderlineTabItem.vue
@@ -47,8 +47,8 @@ onMounted(() => {
                 :class="[
                     vActive
                         ? 'border-spartan-primary-500 text-gray-900'
-                        : 'border-transparent text-gray-400 hover:border-gray-300 hover:text-gray-700',
-                    'group inline-flex items-center gap-1 border-b-2 px-4 py-2 text-sm font-medium',
+                        : 'border-transparent text-gray-400 hover:text-gray-800',
+                    'group inline-flex items-center gap-1 border-b-2 px-4 py-2 text-sm font-medium -mb-px',
                     $props.class,
                 ]"
                 :aria-current="vActive ? 'page' : undefined"
@@ -87,8 +87,8 @@ onMounted(() => {
         :class="[
             vActive
                 ? 'border-spartan-primary-500 text-gray-900'
-                : 'border-transparent text-gray-400 hover:border-gray-300 hover:text-gray-700',
-            'group inline-flex items-center gap-1 border-b-2 px-4 py-2 text-sm font-medium',
+                : 'border-transparent text-gray-400 hover:text-gray-800',
+            'group inline-flex items-center gap-1 border-b-2 px-4 py-2 text-sm font-medium -mb-px',
             $props.class,
         ]"
         :aria-current="vActive ? 'page' : undefined"
