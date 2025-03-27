ddabab48dbc01e813b1e430383ab3e3c8a996e21
Juan Zea <juan.zea@evertecinc.com>
2025-03-27 10:46:47 -0500

    refactor: improve SButton component

--- src/components/spartan/SButton/SButton.vue
+++ src/components/spartan/SButton/SButton.vue
@@ -1,7 +1,8 @@
 <script lang="ts">
 /**
  * A versatile button component with multiple styles and appearances.
- * @see {@link https://github.com/placetopay-org/spartan-vue Github}.
+ * @see {@link https://github.com/placetopay-org/spartan-vue/tree/main/src/components/spartan/SButton Github}.
+ * @see {@link https://develop--646e732a14dfaa707ad59b33.chromatic.com/?path=/docs/buttons-button--docs Storybook}.
  */
 export default {
     name: 'SButton',
@@ -11,43 +12,58 @@ export default {
 <script setup lang="ts">
 import { buttonStyles } from './styles';
 import { twMerge } from 'tailwind-merge';
+import { computed, useSlots, useTemplateRef } from 'vue';
 import type { TButtonProps } from './types';
+import { usePassthrough } from '@/helpers';
 
-const { as = 'button', rounded = 'both', type = 'button', variant = 'primary', size = 'md', leftIcon, icon } = defineProps<TButtonProps>();
+// Props and Defaults
+const props = defineProps<TButtonProps>();
+const { as = 'button', rounded = 'both', variant = 'primary', size = 'md', type, loading, disabled } = props;
+
+// Composables
+const slots = useSlots();
+const refButton = useTemplateRef<HTMLButtonElement>('ref_button');
+
+// Computed properties
+const buttonType = computed(() => (as !== 'button' || type ? type : 'button'));
+const rootClass = computed(() => {
+    const hasText = slots.default?.()[0].children;
+    return twMerge(
+        buttonStyles({
+            variant,
+            rounded,
+            loading,
+            disabled,
+            [hasText ? 'size:text' : 'size:noText']: size,
+        }),
+        props.class,
+    );
+});
+
+// Passthrough
+const { pt, extractor } = usePassthrough();
+const [leftIconClass, leftIconProps] = extractor(pt.value.icon || pt.value.leftIcon);
+const [rightIconClass, rightIconProps] = extractor(pt.value.rightIcon);
+
+// Expose
+defineExpose({ refButton });
 </script>
 
 <template>
-    <component
-        :is="as || 'button'"
-        :ref="$attrs.reference"
-        :type="as && as === 'button' ? type : undefined"
-        :disabled="disabled"
-        :class="
-            twMerge(
-                buttonStyles({
-                    variant,
-                    rounded,
-                    loading,
-                    disabled,
-                    [$slots.default?.()[0].children ? 'size:text' : 'size:noText']: size,
-                }),
-                $props.class
-            )
-        "
-    >
-        <component
-            :is="leftIcon || icon"
-            :class="twMerge('h-5 w-5', $slots.default?.()[0].children && '-ml-0.5')"
-        />
+    <!-- root -->
+    <component :is="as" ref="ref_button" :type="buttonType" :disabled="disabled" :class="rootClass">
+        <!-- icon or leftIcon -->
+        <component data-s-icon v-bind="leftIconProps" :is="leftIcon || icon" :class="twMerge('h-5 w-5', $slots.default?.()[0].children && '-ml-0.5', leftIconClass)" />
+
+        <!-- slot -->
         <slot />
-        <component
-            :is="rightIcon"
-            :class="twMerge('h-5 w-5', $slots.default?.()[0].children && '-mr-0.5' )"
-        />
+
+        <!-- rightIcon -->
+        <component data-s-icon v-bind="rightIconProps" :is="rightIcon" :class="twMerge('h-5 w-5', $slots.default?.()[0].children && '-mr-0.5', rightIconClass)" />
     </component>
 </template>
 
-<style>
+<style scoped>
 .loading {
     color: transparent !important;
     position: relative;
