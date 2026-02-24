65630250d00cd2685288810f02c9955b069c49f1
Juan Zea <juan.zea@evertecinc.com>
2026-02-24 16:22:32 -0500

    feat: add examples for SButton component showcasing left, right, and both icons

---
+++ docs/examples/SButton/left-icon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { CommandSquareIcon } from '@placetopay/iconsax-vue/bold'
+import { usePreview } from '~/composables/usePreview'
+
+usePreview({
+    component: 'SButton',
+    imports: {
+        'CommandSquareIcon': '@placetopay/iconsax-vue/bold',
+    },
+    staticAttrs: {
+        ':left-icon': 'CommandSquareIcon',
+    },
+})
+</script>
+
+<template>
+    <SButton :left-icon="CommandSquareIcon">Left icon</SButton>
+</template>
